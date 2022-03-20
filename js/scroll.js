6fb3ed14821f3b1f2aa39d41113f24cc48593f8f
Ve1okot <85491311+Ve1okot@users.noreply.github.com>
2022-03-20 22:16:26 +0300

    feat: day3

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll('.menu-list__link')
+const btn = document.querySelector('.main__button')
+
+const allLinks = [...links, btn] //...links берёт все элементы массива links
+
+allLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const id = link.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+
+        if (section) {
+            seamless.scrollIntoView(section, { //плавный скролл в т.ч. для Сафари
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            })
+        }
+    })
+})
